a753c4b3e988cec3e68c8401953ed115935edb50
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-02-19 03:12:09 +1300

    Adjusted rotary text animation parameters

--- src/components/HomepageRotaryText.tsx
+++ src/components/HomepageRotaryText.tsx
@@ -16,14 +16,14 @@ export function RotaryText() {
           <TextRotate
             texts={["local", "open-source", "fast"]}
             mainClassName="text-white px-2 sm:px-2 md:px-3 bg-[#ff5941] overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
-            staggerFrom={"last"}
+            staggerFrom={"first"}
             initial={{ y: "100%" }}
             animate={{ y: 0 }}
             exit={{ y: "-120%" }}
             staggerDuration={0.025}
             splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
             transition={{ type: "spring", damping: 30, stiffness: 400 }}
-            rotationInterval={2000}
+            rotationInterval={2500}
           />
           <motion.span
             className="pt-0.5 sm:pt-1 md:pt-2"
